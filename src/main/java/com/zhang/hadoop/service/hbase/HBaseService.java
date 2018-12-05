84d1171d6f1d41b4b8e2a8a4c7e73dbf7d92a856
zhangyufei <2322575520@qq.com>
2018-12-05 17:23:59 +0800

    过滤器

--- src/main/java/com/zhang/hadoop/service/hbase/HBaseService.java
+++ src/main/java/com/zhang/hadoop/service/hbase/HBaseService.java
@@ -10,6 +10,7 @@ import org.apache.hadoop.hbase.util.Bytes;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -21,6 +22,17 @@ public class HBaseService {
     private Configuration configuration;
     private Connection connection;
     private Table table;
+
+//    @Autowired
+//    public HbaseTemplate hbaseTemplate() {
+//        HbaseTemplate hbaseTemplate = new HbaseTemplate();
+//        org.apache.hadoop.conf.Configuration conf = HBaseConfiguration.create();
+//        conf.set("hbase.zookeeper.quorum", "hadoop1");
+//        conf.set("hbase.zookeeper.port", "2181");
+//        hbaseTemplate.setConfiguration(conf);
+//        hbaseTemplate.setAutoFlush(true);
+//        return hbaseTemplate;
+//    }
 
     public void init() throws Exception {
         configuration = HBaseConfiguration.create();
@@ -44,13 +56,14 @@ public class HBaseService {
 //            singleColumnValueFilter();
 //            columnPrefixFilter();
 //            rowKeyFilter();
-            filterList();
+//            filterList();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public void insert() throws Exception {
+        this.init();
         table = connection.getTable(TableName.valueOf("user"));
         Put put = new Put(Bytes.toBytes("2"));
         put.addColumn(Bytes.toBytes("info1"), Bytes.toBytes("name"), Bytes.toBytes("zhangyufei"));
@@ -61,6 +74,7 @@ public class HBaseService {
     }
 
     public void deleteData() throws Exception {
+        this.init();
         table = connection.getTable(TableName.valueOf("user"));
         Delete delete = new Delete(Bytes.toBytes("2"));
         delete.addFamily(Bytes.toBytes("info2"));
@@ -70,6 +84,7 @@ public class HBaseService {
     }
 
     public void queryData() throws Exception {
+        this.init();
         table = connection.getTable(TableName.valueOf("user"));
         Get get = new Get(Bytes.toBytes("1"));
         Result result = table.get(get);
@@ -79,6 +94,7 @@ public class HBaseService {
     }
 
     public void scanData() throws Exception {
+        this.init();
         table = connection.getTable(TableName.valueOf("user"));
         //全表扫描
         Scan scan = new Scan();
@@ -107,6 +123,7 @@ public class HBaseService {
 
     //列值过滤去
     public void singleColumnValueFilter() throws Exception {
+        this.init();
         table = connection.getTable(TableName.valueOf("user"));
         //创建过滤器
         SingleColumnValueExcludeFilter filter = new SingleColumnValueExcludeFilter(
@@ -131,6 +148,7 @@ public class HBaseService {
 
     //列名过滤器
     public void columnPrefixFilter() throws Exception {
+        this.init();
         table = connection.getTable(TableName.valueOf("user"));
         //创建过滤器
         ColumnPrefixFilter filter = new ColumnPrefixFilter(Bytes.toBytes("name"));
@@ -151,6 +169,7 @@ public class HBaseService {
 
     //rowKey过滤器
     public void rowKeyFilter() throws Exception {
+        this.init();
         table = connection.getTable(TableName.valueOf("user"));
         //创建过滤器
         Filter filter = new RowFilter(CompareOperator.EQUAL, new RegexStringComparator("^1"));
@@ -171,6 +190,7 @@ public class HBaseService {
 
     //filterList
     public void filterList() throws Exception {
+        this.init();
         table = connection.getTable(TableName.valueOf("user"));
         //创建过滤器
         FilterList filterList = new FilterList(FilterList.Operator.MUST_PASS_ONE);
@@ -200,9 +220,9 @@ public class HBaseService {
     }
 
     //rowKey过滤器
-    public Map rowKeyFilter(String tableName,String rowKey) throws Exception {
+    public Map rowKeyFilter(String tableName, String rowKey, Map<String, List<String>> title) throws Exception {
         this.init();
-        Map result=new HashMap<>();
+        Map result = new HashMap<>();
         table = connection.getTable(TableName.valueOf(tableName));
         //创建过滤器
         Filter filter = new RowFilter(CompareOperator.EQUAL, new RegexStringComparator(rowKey));
@@ -211,17 +231,31 @@ public class HBaseService {
         scan.setFilter(filter);
         ResultScanner scanner = table.getScanner(scan);
         for (Result row : scanner) {
-            Map result1=new HashMap<>();
-            result1.put("rowKey",Bytes.toString(row.getRow()));
-            byte[] name = row.getValue(Bytes.toBytes("info1"), Bytes.toBytes("name"));
-            result1.put("name",Bytes.toString(name));
-            byte[] age = row.getValue(Bytes.toBytes("info1"), Bytes.toBytes("age"));
-            result1.put("age",Bytes.toString(age));
-            byte[] like = row.getValue(Bytes.toBytes("info2"), Bytes.toBytes("like"));
-            result1.put("like",Bytes.toString(like));
-            result.put(result1.get("rowKey"),result1);
+            Map result1 = new HashMap<>();
+            for (String key : title.keySet()) {
+                Map result2 = new HashMap<>();
+                for (String key1 : title.get(key)) {
+                    byte[] value = row.getValue(Bytes.toBytes(key), Bytes.toBytes(key1));
+                    result2.put(key1, Bytes.toString(value));
+                }
+                result1.put(key, result2);
+            }
+            result.put(Bytes.toString(row.getRow()), result1);
         }
         this.close();
         return result;
+    }
+
+    public void insert(String tableName, String rowKey, Map<String, Map> data) throws Exception {
+        this.init();
+        table = connection.getTable(TableName.valueOf(tableName));
+        Put put = new Put(Bytes.toBytes(rowKey));
+        for (Object key : data.keySet()) {
+            for (Object key1 : data.get(key).keySet()) {
+                put.addColumn(Bytes.toBytes(key.toString()), Bytes.toBytes(key1.toString()), Bytes.toBytes(data.get(key).get(key1).toString()));
+            }
+        }
+        table.put(put);
+        this.close();
     }
 }
