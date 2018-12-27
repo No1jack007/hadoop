f2169318399d164cf80570eb81fa66c627ef4855
No1jack <2322575520@qq.com>
2018-12-27 22:43:09 +0800

    hadoop

---
+++ src/main/java/com/zhang/hadoop/service/kafka/KafkaService.java
@@ -0,0 +1,16 @@
+package com.zhang.hadoop.service.kafka;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by zhang yufei on 2018/12/27.
+ */
+@Service
+public class KafkaService {
+
+    public void test(){
+
+    }
+
+
+}
