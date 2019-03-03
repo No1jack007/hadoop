0e080586aaea0e45951fcbb8a962395a1e57ad73
No1jack <2322575520@qq.com>
2019-03-03 19:07:30 +0800

    hadoop

---
+++ src/main/java/com/zhang/hadoop/scala/Test.scala
@@ -0,0 +1,32 @@
+package com.zhang.hadoop.scala
+
+/**
+  * Created by zhang yufei on 2019/3/3.
+  */
+object Test {
+
+  def main(args: Array[String]) {
+    println("Hello, world!") // 输出 Hello World
+    for (i <- 1 to 3) {
+      println(i);
+    }
+    for (i <- 1 until 3) {
+      println(i);
+    }
+    println(1 - (10));
+    println(m1(2, 3))
+    val r = m1(3, 4)
+    println(r)
+    print(f1(1,2))
+  }
+
+  def m1(a: Int, b: Int) = {
+    a * b
+  }
+
+  var f1=(x: Int,y:Int)=>{
+    x+y
+  }
+
+
+}
