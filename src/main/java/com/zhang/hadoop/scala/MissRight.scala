68e7a6921b1cd8be1eea5ecb5972ec994543f1ae
zhangyufei <2322575520@qq.com>
2019-03-21 19:43:57 +0800

    123

---
+++ src/main/java/com/zhang/hadoop/scala/MissRight.scala
@@ -0,0 +1,33 @@
+package com.zhang.hadoop.scala
+
+import scala.io.Source
+
+
+class MissRight {
+
+  //主构造器
+  //在new的时候主构造器里的内容会先被执行
+  val name="abc"
+  println(name)
+
+  def sayHi={
+    println("hi~")
+  }
+
+  //读取文件
+  try {
+    val source=Source.fromFile("D:\\0-program\\test\\123.txt").mkString
+    println(source)
+  }catch {
+    case e:Exception=>e.printStackTrace()
+  }finally {
+    println("finally")
+  }
+
+}
+
+object MissRight{
+  def main(args: Array[String]): Unit = {
+    var m=new MissRight
+  }
+}
--- src/main/java/com/zhang/hadoop/scala/People.scala
+++ src/main/java/com/zhang/hadoop/scala/People.scala
@@ -1,14 +1,15 @@
 package com.zhang.hadoop.scala
 
-class People(val id:String,var name:String) {
+class People(val id:String,var name:String,var age:Int=18) {
 
 }
 
 object People{
   def main(args: Array[String]): Unit = {
-      val p=new People("123","abc")
+      val p=new People("123","abc",20)
       println(p.id)
       p.name="itcast"
       println(p.name)
+      println(p.age)
   }
 }
