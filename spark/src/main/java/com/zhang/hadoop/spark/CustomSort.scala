fa9eb59da39f9d58a7b720be6e8a4e28681f1a9e
zhangyuei <2322575520@qq.com>
2019-06-09 16:42:15 +0800

    spark

--- spark/src/main/java/com/zhang/hadoop/spark/CustomSort.scala
+++ spark/src/main/java/com/zhang/hadoop/spark/CustomSort.scala
@@ -2,14 +2,32 @@ package com.zhang.hadoop.spark
 
 import org.apache.spark.{SparkConf, SparkContext}
 
+object OrderContext {
+
+  implicit val girlOrdering=new Ordering[Girl_1] {
+    override def compare(x: Girl_1, y: Girl_1): Int = {
+      if (x.faceValue > y.faceValue) 1
+      else if (x.faceValue == y.faceValue) {
+        if (x.age > y.age) -1 else 1
+      } else {
+        -1
+      }
+    }
+  }
+
+}
+
 object CustomSort {
 
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("CustomSort").setMaster("local[2]")
     val sc = new SparkContext(conf)
     val rdd1 = sc.parallelize(List(("yuihatao", 90, 28, 1), ("angelababy", 90, 27, 2), ("JuJingYi", 95, 22, 3)))
-    val rdd2 = rdd1.sortBy(x => Girl(x._2, x._3),false)
+    val rdd2 = rdd1.sortBy(x => Girl(x._2, x._3), false)
     println(rdd2.collect().toBuffer)
+    import OrderContext._
+    val rdd3 = rdd1.sortBy(x => Girl_1(x._2, x._3), false)
+    println(rdd3.collect().toBuffer)
     sc.stop()
   }
 
@@ -25,8 +43,5 @@ case class Girl(val faceValue: Int, val age: Int) extends Ordered[Girl] with Ser
   }
 }
 
-case class Girl1(faceValue:Int,age:Int) extends Serializable
+case class Girl_1(faceValue: Int, age: Int) extends Serializable
 
-object OrderContext{
-
-}
