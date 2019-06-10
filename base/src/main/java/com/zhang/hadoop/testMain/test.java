007561eff4d8837d3c511ad4655c3b3a25469cec
zhangyufei <2322575520@qq.com>
2019-06-10 09:11:04 +0800

    scala

--- base/src/main/java/com/zhang/hadoop/testMain/test.java
+++ base/src/main/java/com/zhang/hadoop/testMain/test.java
@@ -16,5 +16,8 @@ public class test {
         String date="2018.10";
         String a1[]=date.split("\\.");
         System.out.println(a1[0]+"\t"+a1[1]);
+
+        Pattern p1 = Pattern.compile("^[\\u4e00-\\u9fa5A-Za-z()（）]+");
+        System.out.println(p1.matcher("三星SDI匈牙利制造和经销封闭式股份公司").matches());
     }
 }
