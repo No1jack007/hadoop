5106adab13cddefbff58c6eec3f0dcd0737c5059
zhangyufei <2322575520@qq.com>
2019-06-17 18:05:48 +0800

    scala

--- spark/src/main/java/com/zhang/hadoop/sparkStreaming/StreamingWordCount.scala
+++ spark/src/main/java/com/zhang/hadoop/sparkStreaming/StreamingWordCount.scala
@@ -1,5 +1,25 @@
 package com.zhang.hadoop.sparkStreaming
 
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
 object StreamingWordCount {
+
+  def main(args: Array[String]): Unit = {
+    //StreamingContext
+    val conf = new SparkConf().setAppName("StreamingWordCount").setMaster("local[2]")
+    val sc = new SparkContext(conf)
+    val ssc = new StreamingContext(sc, Seconds(5))
+    //接收数据
+    val ds = ssc.socketTextStream("hadoop1-1", 8888)
+    //DStream是一个特殊的RDD
+    val result=ds.flatMap(_.split("  ")).map((_, 1)).reduceByKey(_ + _)
+    //打印结果
+    result.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
 
 }
