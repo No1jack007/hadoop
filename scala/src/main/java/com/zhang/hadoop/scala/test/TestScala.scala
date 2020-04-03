ad9160154e92e58135b30c92df0d6db7185f8ef1
zhangyufei <2322575520@qq.com>
2020-04-03 17:42:01 +0800

    scala

--- scala/src/main/java/com/zhang/hadoop/scala/test/TestScala.scala
+++ scala/src/main/java/com/zhang/hadoop/scala/test/TestScala.scala
@@ -1,5 +1,7 @@
 package com.zhang.hadoop.scala.test
 
+import scala.util.Random
+
 object TestScala {
 
   def main(args: Array[String]): Unit = {
@@ -8,6 +10,22 @@ object TestScala {
     val listA = List(1,2,3)
 
     println(listA.foldLeft(0)((sum,i)=>sum+i))
+
+
+    val l1=List(1,2,3)
+    val l2=List(1,2,3)
+    println(l1==l2)
+
+    val names=Array("Akiho Yoshizawa","YuiHatano","Aoi Sola")
+    val name=names(Random.nextInt(names.length))
+
+    name match {
+      case "Akiho Yoshizawa" => println("吉老师。。。")
+      case "YuiHatano"=> println("波老师。。。")
+      case _ => println("真不知道你们在说什么")
+    }
+
+
 
   }
 
